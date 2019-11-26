b7a53c3a4829db1e58c77cd7f0fd3021a1282241
Nastya Kulakova <nvk@8xx8.ru>
2019-11-26 19:51:12 +0400

    Solution for exercise fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,41 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        tail = self
+        begin
+          head, *tail = tail
+          yield head
+        end until tail.empty?
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        my_reduce(MyArray.new) do |acc, el|
+          acc << yield(el)
+        end
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        my_reduce(MyArray.new) do |acc, el|
+          if el.nil?
+            acc
+          else
+            acc << el
+          end
+        end
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        tail = self
+        acc, *tail = tail if acc.nil?
+        begin
+          head, *tail = tail
+          acc = yield acc, head
+        end until tail.empty?
+        acc
       end
     end
   end
